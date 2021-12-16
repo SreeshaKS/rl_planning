a709b7fc6c37e47f12b68082611625f18e35132d
Sreesha Kuruvadi <sreeshakuruvadi@Sreeshas-MacBook-Pro.local>
2021-12-16 02:26:10 -0500

    implemented mcts skeleton

--- mcts/algorithm/mcts.py
+++ mcts/algorithm/mcts.py
@@ -1,11 +1,43 @@
+class Bounds:
+    def __init__(
+        self,
+        arena_bounds
+        , n_iterations_bound
+        , rollout_bounds
+        , num_primitive_actions
+        , animation_duration
+    ):
+        self.arena_bounds = arena_bounds
+        self.n_iterations_bound = n_iterations_bound
+        self.rollout_bounds = rollout_bounds
+        self.num_primitive_actions = num_primitive_actions
+        self.animation_duration = animation_duration
+
+
 class MCTS:
     def __init__(
             self,
-            actions,
-            n_iterations_bound,
-            rollout_bound
+            algorithm_bounds,
         ):
+        self.algorithm_bounds = algorithm_bounds
+    
+    def run(self, step, rewards, arena):
         pass
 
-    def run(self, node):
+    def _select(self):
         pass
+    
+    def _expand(self):
+        pass
+    
+    def _simulate(self):
+        pass
+    
+    def _backpropagate(self):
+        pass
+
+    def rollout(self):
+        pass
+
+
+    
